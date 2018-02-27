6f36840ea1fc8f19622009cb24347a99bfe8ab62
amasclans <albert.masclans@globalzitro.com>
2018-02-27 17:34:40 +0100

    split lines

--- frontend/test/unit/MainView.spec.js
+++ frontend/test/unit/MainView.spec.js
@@ -5,7 +5,13 @@ import MainView from "@/views/Main.vue";
 
 describe("MainView", () => {
   it("renders a main-view element", () => {
-    const wrapper = shallow(MainView, { stubs: { 'router-link': { render: h => h("a") } } });
+    const wrapper = shallow(MainView, {
+      stubs: {
+        'router-link': {
+          render: h => h("a")
+        }
+      }
+    });
     expect(wrapper.contains("#main-view")).to.be.true;
   });
 });
