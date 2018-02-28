38dc3dd3017138d2f3b4a3de768d20e003367d83
amasclans <albert.masclans@globalzitro.com>
2018-02-28 16:50:04 +0100

    Solved different initial card value

--- backend/src/configs.clj
+++ backend/src/configs.clj
@@ -13,4 +13,14 @@
 (defn ini-hand
   "Generates de initial hand (can't be empty)"
   []
-  (vec (repeat 13 {:power 11})))
+  [{:power 5}
+   {:power 6}
+   {:power 7}
+   {:power 8}
+   {:power 9}
+   {:power 10}
+   {:power 11}
+   {:power 12}
+   {:power 13}
+   {:power 14}
+   {:power 15}])
