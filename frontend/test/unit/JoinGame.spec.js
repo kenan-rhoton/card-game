fcfc45cdbf15d5d66e03ff2ba1ad823cddf19653
Kenan Rhoton <kenan.rhoton@gmail.com>
2018-03-01 12:58:56 +0100

    Made things more testable

--- frontend/test/unit/JoinGame.spec.js
+++ frontend/test/unit/JoinGame.spec.js
@@ -1,9 +1,26 @@
+import { expect } from "chai";
 import { shallow } from "@vue/test-utils";
 import JoinGame from "@/components/JoinGame.vue";
 
 describe("JoinGame", () => {
   it("renders", () => {
-    const $route = { params: { gameID: 5 } };
-    shallow(JoinGame, { mocks: { $route } });
+    var pushParams = {};
+    const $route = { params: { joinID: 5 } };
+    const $router = { push: a => (pushParams = a) };
+    shallow(JoinGame, {
+      mocks: { $route, $router },
+      methods: {
+        fetchGame: () => {
+          return { data: { "player-id": 3 } };
+        }
+      }
+    });
+    expect(pushParams).to.eql({
+      name: "Game",
+      params: {
+        gameID: 5,
+        playerID: 3
+      }
+    });
   });
 });
