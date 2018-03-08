2c81e4c5b212b991162d582e6ab3758c35aab19c
Kenan Rhoton <kenan.rhoton@gmail.com>
2018-03-08 15:10:38 +0100

    Working config everywhere

--- frontend/lobby/waiting/waiting.js
+++ frontend/lobby/waiting/waiting.js
@@ -5,6 +5,17 @@ const gameID = params.get("gameID");
 const playerID = params.get("playerID");
 const gameURL = `/game/?gameID=${gameID}&playerID=${playerID}`;
 
+var config = {}
+
+var req = new XMLHttpRequest();
+req.open("GET", "/config/data/config.yml");
+req.responseType = "text";
+req.onload = function () {
+    config = jsyaml.load(req.response);
+}
+req.send();
+
+
 function checkForOpponent() {
     var req = new XMLHttpRequest();
     req.open("GET", `http://backend:3000/games/${gameID}/player/${playerID}`);
@@ -13,7 +24,7 @@ function checkForOpponent() {
     req.onload = function () {
         const status = req.response["status"];
         
-        if (status !== "Waiting for an opponent") {
+        if (status !== config.messages["no-opp"]) {
             window.location.href = gameURL;
         }
     }
