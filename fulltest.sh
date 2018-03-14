ec833e52c0a5ab5fd6ba839a1160c13b5c7cf254
Kenan Rhoton <kenan.rhoton@gmail.com>
2018-03-14 07:06:06 +0100

    Split out e2e tests from the Main Docker Compose

--- fulltest.sh
+++ fulltest.sh
@@ -1,4 +1,4 @@
 docker-compose down
 docker-compose build
 docker-compose run backend lein test
-docker-compose run e2e test-delayed
+docker-compose -f docker-compose.yml -f docker-compose.e2e.yml run e2e test-delayed
