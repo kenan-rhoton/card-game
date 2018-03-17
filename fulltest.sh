6498cbfebdb6530a0e4487861d402e6402eaffd7
Kenan Rhoton <kenan.rhoton@gmail.com>
2018-03-17 14:20:53 +0100

    Added preparation step before E2E tests

--- fulltest.sh
+++ fulltest.sh
@@ -2,4 +2,5 @@ set -e # Stop the script on any failure or error
 docker-compose -f docker-compose.yml -f docker-compose.e2e.yml down
 docker-compose -f docker-compose.yml -f docker-compose.e2e.yml build
 docker-compose run backend lein test
+docker-compose run frontend yarn install
 docker-compose -f docker-compose.yml -f docker-compose.e2e.yml run e2e test
