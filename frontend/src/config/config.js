87d29a9e63d001008e3a98b429efff0b84df7aa4
Kenan Rhoton <kenan.rhoton@gmail.com>
2018-03-22 10:03:56 +0100

    Properly mocked config in frontend

--- frontend/src/config/config.js
+++ frontend/src/config/config.js
@@ -1,14 +1,1 @@
-try {
-    var config = require('./data/config.yml');
-} catch(error) {
-    if (error.code !== "MODULE_NOT_FOUND") {
-        throw error;
-    }
-    config = {
-        servers: {
-            backend: "potato",
-        }
-    };
-}
-
-module.exports =  config;
+module.exports = require('./data/config.yml');
