770238a7e2d946682b538a3c40c6c94f13288d69
amasclans <albert.masclans@globalzitro.com>
2018-03-28 08:10:12 +0200

    single-argument ini-configs

--- backend/src/rules/create_game.clj
+++ backend/src/rules/create_game.clj
@@ -11,14 +11,15 @@
 
 (defn new-game
   "Creates a new game object"
-  [& ini-config]
-  {
-   :players (let [hands (:hands (first ini-config) hands/default-hands)]
-              [(new-player (first hands))
-               (new-player (second hands))])
-   :rows (vec (reduce
-                #(concat %1 [{:limit %2 :cards []}])
-                []
-                (:limits (first ini-config) rows/default-limits)))
-   :next-play [nil nil]
-   })
+  ([] (new-game {}))
+  ([ini-config]
+   {
+    :players (let [hands (:hands ini-config hands/default-hands)]
+               [(new-player (first hands))
+                (new-player (second hands))])
+    :rows (vec (reduce
+                 #(concat %1 [{:limit %2 :cards []}])
+                 []
+                 (:limits ini-config rows/default-limits)))
+    :next-play [nil nil]
+   }))
