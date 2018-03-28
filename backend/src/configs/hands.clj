cfee1e7d29213195d89c6165c71a84ef49a7e542
amasclans <albert.masclans@globalzitro.com>
2018-03-28 08:18:34 +0200

    new config

--- backend/src/configs/hands.clj
+++ backend/src/configs/hands.clj
@@ -2,15 +2,11 @@
 
 (def default-hand
   "Default hand (can't be empty)"
-  [{:power 5}
-   {:power 6}
-   {:power 7}
-   {:power 8}
+  [{:power 10}
    {:power 9}
-   {:power 10}
-   {:power 11}
-   {:power 12}
-   {:power 13}
-   {:power 14}
-   {:power 15}
-   {:power 16}]) 
+   {:power 8}
+   {:power 7}
+   {:power 6 :add-power 1}
+   {:power 6 :add-power -1}
+   {:power 4 :add-power 2}
+   {:power 4 :add-power -2}])
