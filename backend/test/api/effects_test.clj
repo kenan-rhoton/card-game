d8b46c641c404c51758dc7cc92b1642b395949c1
amasclans <albert.masclans@globalzitro.com>
2018-04-03 12:37:23 +0200

    ups!

--- backend/test/api/effects_test.clj
+++ backend/test/api/effects_test.clj
@@ -10,14 +10,14 @@
   ; Can play cards with effects and they act as expected
   (expect
     -100
-    (let [game (api/create-game {:hand [{:power 0} {:power 10 :add-power -100} {:power 10}]})
+    (let [game (api/create-game {:hands [[{:power 0} {:power 100}]
+                                         [{:power 1} {:power 10 :add-power -100}]]})
           game-id (:game-id game)
           player-id (:player-id game)
           opponent-id (:player-id (api/add-player game-id))]
       (api/play-card-as-player game-id player-id   0 0)
       (api/play-card-as-player game-id opponent-id 0 1)
-      (api/play-card-as-player game-id player-id   1 1)
-      (println (api/get-game game-id player-id))
+      (api/play-card-as-player game-id player-id   0 1)
       (api/play-card-as-player game-id opponent-id 0 1 [:rows 0 :cards 0])
       (get-in (api/get-game game-id player-id)
               [:rows 0 :cards 0 :power]))))
