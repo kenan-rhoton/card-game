8531aa570612a94e32f36798b90212b6b706dd51
amasclans <albert.masclans@globalzitro.com>
2018-04-16 15:29:29 +0200

    changed var idiotic names

--- frontend/src/game/board/builder.js
+++ frontend/src/game/board/builder.js
@@ -7,7 +7,7 @@ const oppRows = document.getElementById("opp-rows");
 
 const baseScore = document.getElementById("score-template")
     .content.querySelector("div");
-const row-scores = document.getElementById("row-scores");
+const rowScores = document.getElementById("row-scores");
 const limits = document.getElementById("limits");
 
 module.exports = {
@@ -26,7 +26,7 @@ module.exports = {
 
             var newScore = baseScore.cloneNode(true);
             newScore.setAttribute("rownum", i);
-            row-scores.appendChild(newScore);
+            rowScores.appendChild(newScore);
 
             var newScore2 = baseScore.cloneNode(true);
             newScore2.setAttribute("rownum", i);
