0c8653d26908560892cc27d372f5d3c309b18ed0
amasclans <albert.masclans@globalzitro.com>
2018-04-24 10:08:20 +0200

    mocking status for test

--- frontend/tests/game/play.test.js
+++ frontend/tests/game/play.test.js
@@ -4,6 +4,9 @@ const play = require('game/play/play.js');
 
 test('Playing cards changes background as intended', () => {
     var card = document.createElement("div");
+    var mockedStatus = document.createElement("div");
+    mockedStatus.setAttribute("id", "game-status");
+    document.body.appendChild(mockedStatus);
 
     card.setAttribute("add-power", "undefined");
     play.playCard(card);
