c77d7cc4c6bf5cd5a3544076002a04f993daec5a
amasclans <albert.masclans@globalzitro.com>
2018-09-19 11:53:45 +0200

    (be) abilities as functions

---
+++ backend/src/rules/abilities.clj
@@ -0,0 +1,16 @@
+(ns rules.abilities)
+
+(defn add-power
+  "Creates the ability function"
+  [increase]
+  (fn    
+;    "Alters a cards' power, by adding the a defined value to it.
+;    This power may be negative, resulting in a decrease"
+    [game-state target]
+      (update-in
+        game-state
+        [:cards target]
+        #(merge % {:power
+                   (+
+                    (get-in game-state [:cards target :power])
+                    increase)}))))
