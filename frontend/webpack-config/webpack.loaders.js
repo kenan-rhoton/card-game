6ad8c6888fb1dd348150485c1e2f10f1435f0a84
kolle <kolle.redsauce@gmail.com>
2018-09-23 20:53:56 +0200

    trying to reformat things and add sass

--- frontend/webpack-config/webpack.loaders.js
+++ frontend/webpack-config/webpack.loaders.js
@@ -14,6 +14,18 @@ module.exports = {
                     { loader: 'json-loader' },
                     { loader: 'yaml-loader' }
                 ]
+            },
+            {
+                test: /\.css$/,
+                use: [ 'style-loader', 'css-loader' ]
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    "style-loader", // creates style nodes from JS strings
+                    "css-loader", // translates CSS into CommonJS
+                    "sass-loader" // compiles Sass to CSS, using Node Sass by default
+                ]
             }
         ]
     }
